c0460d9bd2bc5fa700a6b9320b76adee4e4afcc7
omar-ahmd <omaharmad@gmail.com>
2020-10-07 20:36:16 +0300

    Contact Page done

---
+++ behavior.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var thumbnailElement1 = document.getElementById("smart_thumbnail1");
+    var thumbnailElement2 = document.getElementById("smart_thumbnail2");
+    var thumbnailElement3 = document.getElementById("smart_thumbnail3");
+    var thumbnailElement4 = document.getElementById("smart_thumbnail4");
+    var thumbnailElement5 = document.getElementById("smart_thumbnail5");
+    var thumbnailElement6 = document.getElementById("smart_thumbnail6");
+    var thumbnailElement7 = document.getElementById("smart_thumbnail7");
+    var thumbnailElement8 = document.getElementById("smart_thumbnail8");
+    var thumbnailElement9 = document.getElementById("smart_thumbnail9");
+
+
+    thumbnailElement1.addEventListener("click", function() {
+        if (thumbnailElement1.className == "") {
+            thumbnailElement1.className = "small"
+        } else {
+            thumbnailElement1.className = ""
+        }
+    });
+
+    thumbnailElement2.addEventListener("click", function() {
+        if (thumbnailElement2.className == "") {
+            thumbnailElement2.className = "small"
+        } else {
+            thumbnailElement2.className = ""
+        }
+    });
+
+    thumbnailElement3.addEventListener("click", function() {
+        if (thumbnailElement3.className == "") {
+            thumbnailElement3.className = "small"
+        } else {
+            thumbnailElement3.className = ""
+        }
+    });
+
+    thumbnailElement4.addEventListener("click", function() {
+        if (thumbnailElement4.className == "") {
+            thumbnailElement4.className = "small"
+        } else {
+            thumbnailElement4.className = ""
+        }
+    });
+
+    thumbnailElement5.addEventListener("click", function() {
+        if (thumbnailElement5.className == "") {
+            thumbnailElement5.className = "small"
+        } else {
+            thumbnailElement5.className = ""
+        }
+    });
+
+    thumbnailElement6.addEventListener("click", function() {
+        if (thumbnailElement6.className == "") {
+            thumbnailElement6.className = "small"
+        } else {
+            thumbnailElement6.className = ""
+        }
+    });
+
+    thumbnailElement7.addEventListener("click", function() {
+        if (thumbnailElement7.className == "") {
+            thumbnailElement7.className = "small"
+        } else {
+            thumbnailElement7.className = ""
+        }
+    });
+
+    thumbnailElement8.addEventListener("click", function() {
+        if (thumbnailElement8.className == "") {
+            thumbnailElement8.className = "small"
+        } else {
+            thumbnailElement8.className = ""
+        }
+    });
+
+
+    thumbnailElement9.addEventListener("click", function() {
+        if (thumbnailElement9.className == "") {
+            thumbnailElement9.className = "small"
+        } else {
+            thumbnailElement9.className = ""
+        }
+    });
+
+
+
+
+});
