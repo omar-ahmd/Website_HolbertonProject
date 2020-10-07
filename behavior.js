3eeb3e755548ceb9e6d53a521fcadf4879a78ca7
omar-ahmd <omaharmad@gmail.com>
2020-10-07 22:58:21 +0300

    edit speed of resizing for some images

--- behavior.js
+++ behavior.js
@@ -12,75 +12,75 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
     thumbnailElement1.addEventListener("click", function() {
-        if (thumbnailElement1.className == "") {
-            thumbnailElement1.className = "small"
+        if (thumbnailElement1.className == "imgg") {
+            thumbnailElement1.className = "small imgg"
         } else {
-            thumbnailElement1.className = ""
+            thumbnailElement1.className = "imgg"
         }
     });
 
     thumbnailElement2.addEventListener("click", function() {
-        if (thumbnailElement2.className == "") {
-            thumbnailElement2.className = "small"
+        if (thumbnailElement2.className == "imgg") {
+            thumbnailElement2.className = "small imgg"
         } else {
-            thumbnailElement2.className = ""
+            thumbnailElement2.className = "imgg"
         }
     });
 
     thumbnailElement3.addEventListener("click", function() {
-        if (thumbnailElement3.className == "") {
-            thumbnailElement3.className = "small"
+        if (thumbnailElement3.className == "imgg") {
+            thumbnailElement3.className = "small imgg"
         } else {
-            thumbnailElement3.className = ""
+            thumbnailElement3.className = "imgg"
         }
     });
 
     thumbnailElement4.addEventListener("click", function() {
-        if (thumbnailElement4.className == "") {
-            thumbnailElement4.className = "small"
+        if (thumbnailElement4.className == "imgg") {
+            thumbnailElement4.className = "small imgg"
         } else {
-            thumbnailElement4.className = ""
+            thumbnailElement4.className = "imgg"
         }
     });
 
     thumbnailElement5.addEventListener("click", function() {
-        if (thumbnailElement5.className == "") {
-            thumbnailElement5.className = "small"
+        if (thumbnailElement5.className == "imgg") {
+            thumbnailElement5.className = "small imgg"
         } else {
-            thumbnailElement5.className = ""
+            thumbnailElement5.className = "imgg"
         }
     });
 
     thumbnailElement6.addEventListener("click", function() {
-        if (thumbnailElement6.className == "") {
-            thumbnailElement6.className = "small"
+        if (thumbnailElement6.className == "imgg") {
+            thumbnailElement6.className = "small imgg"
         } else {
-            thumbnailElement6.className = ""
+            thumbnailElement6.className = "imgg"
         }
     });
 
     thumbnailElement7.addEventListener("click", function() {
-        if (thumbnailElement7.className == "") {
-            thumbnailElement7.className = "small"
+        if (thumbnailElement7.className == "imgg") {
+            thumbnailElement7.className = "small imgg"
         } else {
-            thumbnailElement7.className = ""
+            thumbnailElement7.className = "imgg"
         }
     });
 
     thumbnailElement8.addEventListener("click", function() {
-        if (thumbnailElement8.className == "") {
-            thumbnailElement8.className = "small"
+        if (thumbnailElement8.className == "imgg") {
+            thumbnailElement8.className = "small imgg"
         } else {
-            thumbnailElement8.className = ""
+            thumbnailElement8.className = "imgg"
         }
     });
 
 
     thumbnailElement9.addEventListener("click", function() {
-        if (thumbnailElement9.className == "") {
-            thumbnailElement9.className = "small"
+        if (thumbnailElement9.className == "imgg") {
+            thumbnailElement9.className = "small imgg"
         } else {
-            thumbnailElement9.className = ""
+            thumbnailElement9.className = "imgg"
         }
     });
 
